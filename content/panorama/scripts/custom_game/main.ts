1c3bb3722686a1d28785f618212cf69e07afa2e6
litianxing <837727384@qq.com>
2022-06-08 11:33:02 +0800

    npm version update

--- content/panorama/scripts/custom_game/main.ts
+++ content/panorama/scripts/custom_game/main.ts
@@ -2,7 +2,7 @@
 import { mainhud_mainhud } from "./view/mainhud/mainhud_mainhud";
 import { package1_login } from "./view/package1/package1_login";
 import { package1_testui } from "./view/package1/package1_testui";
-import { test_HeroSelectView_data } from "./view/test/test_data";
+import { test_HeroSelectView } from "./view/test/test_HeroSelectView";
 import { ViewManager } from "./ViewManager";
 
 class ExampleUI {
@@ -15,7 +15,7 @@ class ExampleUI {
         ViewManager.Get().clearTempNode();
         // ViewManager.Get().open(package1_testui);
         // ViewManager.Get().open(mainhud_mainhud);
-        ViewManager.Get().open(test_HeroSelectView_data);
+        ViewManager.Get().open(test_HeroSelectView);
         // var panel = $.CreatePanel('Panel', $.GetContextPanel(), 'compile_helper');
         // panel.BLoadLayout("file://{resources}/layout/custom_game/fgui/compile_helper.xml",false,false);
     }
